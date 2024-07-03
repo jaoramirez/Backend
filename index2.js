594cca87d1b90095a4d1b1b0c751073ee2b3a8f0
jaoramirez <149978668+jaoramirez@users.noreply.github.com>
2024-07-03 11:59:37 -0300

    Add files via upload

---
+++ index2.js
@@ -0,0 +1,9 @@
+/* for(let i = 1; i <= 10; i++){
+    console.log(i*5)}*/
+
+
+/*const readlineSync = require('readline-sync')
+ let valor = readlineSync.question('Digite um numero: ')
+ for(let i = 1; i <= 10; i++){
+    console.log(valor*i)
+ }*/
